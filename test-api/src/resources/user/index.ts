2f5aec532a9c80ba51044db4e597323ef7a73853
Shubham Kulkarni <shubhamkulkarni.sh@gmail.com>
2025-09-15 15:56:45 +0530

    fix

--- test-api/src/resources/user/index.ts
+++
@@ -1,6 +0,0 @@
-import { Application } from 'express';
-import userRoutes from './routes';
-
-export default function registerUserRoutes(app: Application): void {
-  app.use('/api/users', userRoutes);
-}
